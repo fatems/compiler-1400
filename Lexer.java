8f6448560497dc429f28c283afcaac8c145d637f
fatems <46194089+fatems@users.noreply.github.com>
2021-06-10 01:59:45 +0430

    Update Lexer.java

--- Lexer.java
+++ Lexer.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -25,10 +26,20 @@ public class Lexer {
 	 List<Token> result;
 
 	 String src;
+	 
+	 String oldestStackElement;
+	 Stack<String> stack = new Stack<String>();
+	 
+	 String SymbolResult;
+	 
+	 //used for printing symbol table
+	 
+	 
 	/**
 	 * Initializes a newly created {@code Lexer} object
 	 */
 	public Lexer() {
+		stack.push("");
 		regEx = new TreeMap<TokenType, String>();
 		launchRegEx();
 		result = new ArrayList<Token>();
@@ -53,7 +64,7 @@ public class Lexer {
 				result.add(token);
 			}
 		} while (token != null && position != source.length());
-		if (position != source.length()) {
+		if (position > source.length()) {
 			throw new AnalyzerException("Lexical error at position # "+ position, position);
 
 		}
@@ -96,23 +107,199 @@ public class Lexer {
 	 * 
 	 */
 	private Token separateToken(String source, int fromIndex) {
+		int ii=0;
 		if (fromIndex < 0 || fromIndex >= source.length()) {
 			throw new IllegalArgumentException("Illegal index in the input stream!");
 		}
 		for (TokenType tokenType : TokenType.values()) {
 			//System.out.println(tokenType);
-			Pattern p = Pattern.compile(".{" + fromIndex + "}"  + regEx.get(tokenType),
-					Pattern.DOTALL);
-			Matcher m = p.matcher(source );
-			if (m.matches()) {
-				String lexema = m.group(1);
-				System.out.println(tokenType);
-				
-				return new Token(fromIndex, fromIndex + lexema.length(), lexema, tokenType);
+			System.out.println(ii +"the stack's peek is: "+stack.peek());
+			if(stack.lastElement()=="Int"||
+					stack.peek()=="Double"||
+					stack.peek()=="Float"||
+					stack.peek()=="String"
+					) {
+				ii++;
+				if(tokenType.toString().equals("Identifier")) {
+					Pattern p = Pattern.compile(".{" + fromIndex + "}"  + regEx.get(tokenType),
+							Pattern.DOTALL);
+					Matcher m = p.matcher(source );
+					
+					if (m.matches()) {
+						String lexema = m.group(1);
+						//
+						stack.push(tokenType.toString());
+							
+							System.out.println("this is what lexemais : "+lexema);
+						return new Token(fromIndex, fromIndex + lexema.length(), lexema, tokenType);
+					
+					}
+					
+				}
 			}
-		}
+			else if(stack.peek().equals("Identifier")) {
+				
+				///////// alternative : find ig
+				if(tokenType.toString().equals("Assignment")) {
+					Pattern p = Pattern.compile(".{" + fromIndex + "}"  + regEx.get(tokenType),
+							Pattern.DOTALL);
+					Matcher m = p.matcher(source );
+					
+					if (m.matches()) {
+						String lexema = m.group(1);
+						//
+						stack.push(tokenType.toString());
+						
+						
+						return new Token(fromIndex, fromIndex + lexema.length(), lexema, tokenType);
+					
+					}
+					
+				}
+			}
+			
+			else if(stack.peek().equals("Assignment")) {
+				
+				///////// alternative : find ig
+				if(tokenType.toString().equals("IntValue") && oldestStackElement =="Int" ) {
+					Pattern p = Pattern.compile(".{" + fromIndex + "}"  + regEx.get(tokenType),
+							Pattern.DOTALL);
+					Matcher m = p.matcher(source );
+					
+					if (m.matches()) {
+						String lexema = m.group(1);
+						System.out.println(lexema);
+						stack.push(tokenType.toString());
+						
+						
+						printStack();
+						
+						return new Token(fromIndex, fromIndex + lexema.length(), lexema, tokenType);
+					
+					}
+					
+				}
+				else if(tokenType.toString().equals("StringValue") &&  oldestStackElement =="String") {
+					Pattern p = Pattern.compile(".{" + fromIndex + "}"  + regEx.get(tokenType),
+							Pattern.DOTALL);
+					Matcher m = p.matcher(source );
+					
+					if (m.matches()) {
+						String lexema = m.group(1);
+						//
+						stack.push(tokenType.toString());
+						
+						
+						printStack();
+						
+						return new Token(fromIndex, fromIndex + lexema.length(), lexema, tokenType);
+					
+					}
+					
+				}
+				else if(tokenType.toString().equals("FloatDouble") && ( oldestStackElement =="Float"|| oldestStackElement =="Double")) {
+					Pattern p = Pattern.compile(".{" + fromIndex + "}"  + regEx.get(tokenType),
+							Pattern.DOTALL);
+					Matcher m = p.matcher(source );
+					
+					if (m.matches()) {
+						String lexema = m.group(1);
+						//
+						stack.push(tokenType.toString());
+						
+						printStack();
+						
+						return new Token(fromIndex, fromIndex + lexema.length(), lexema, tokenType);
+					
+					}
+					
+				}
+				else {
+					stack.push("Undefined");
+					printStack();
+				}
+				
+			}
+			
+			else {
+				Pattern p = Pattern.compile(".{" + fromIndex + "}"  + regEx.get(tokenType),
+						Pattern.DOTALL);
+				
+				Matcher m = p.matcher(source );
+				
+				
+//				if(p.matches("Keyword")) {
+//					
+//				}
+				if (m.matches()) {
+					String lexema = m.group(1);
+					System.out.println("dummy its "+tokenType);
+					if(tokenType.toString().equals("Int") ||
+							tokenType.toString().equals("Double")||
+							tokenType.toString().equals("Float")||
+							tokenType.toString().equals("String")) {
+						stack.push(tokenType.toString());
+						//System.out.println("wth"+stack.peek());
+						
+						oldestStackElement=tokenType.toString();
+						
+					}
+					return new Token(fromIndex, fromIndex + lexema.length(), lexema, tokenType);
+					
+				}
+			}
 
+		}
+			
 		return null;
+	}
+	
+//	public Token matchTokens(TokenType tokenType, int fromIndex, String source, TokenType current, TokenType last) {
+//		if(tokenType.toString().equals(current) && stack.get(0).equals(last)) {
+//			Pattern p = Pattern.compile(".{" + fromIndex + "}"  + regEx.get(tokenType),
+//					Pattern.DOTALL);
+//			Matcher m = p.matcher(source );
+//			
+//			if (m.matches()) {
+//				String lexema = m.group(1);
+//				//
+//				stack.push(tokenType.toString());
+//				
+//				lastStackItem=lexema;
+//				printStack();
+//				
+//				return new Token(fromIndex, fromIndex + lexema.length(), lexema, tokenType);
+//			
+//			}
+//			
+//		}
+//		return null;
+//	}
+	
+	public void printStack() {
+		Stack<String> temp = new Stack<String>();
+		SymbolResult += "( ";
+		while (stack.empty() == false)
+		  {
+		    temp.push(stack.peek());
+		    stack.pop();
+		    
+		  }  
+		while(temp.empty() == false) 
+		{
+			String s = temp.peek();
+			System.out.println(s + "  " );
+			
+			SymbolResult += s;
+			SymbolResult += "  ";
+			
+			temp.pop();
+		}
+		SymbolResult += ")\n";
+		
+		stack.push(" ");
+		
+		
 	}
 
 	/**
@@ -123,80 +310,87 @@ public class Lexer {
 		
 		
 		//change the place of identifier later and add .* to the first of all these
-		regEx.put(TokenType.Int, "\\b(int)\\b.*");
 		
 		
+		regEx.put(TokenType.WhiteSpace, ".*( ).*");
+		//regEx.put(TokenType.Keyword,".*\\b(int|double|float|string)\\b.*");
+		regEx.put(TokenType.Int, ".*\\b(int)\\b.*");
 		
-		
-		regEx.put(TokenType.Identifier, ".*\\b([a-zA-Z]{1}[0-9a-zA-Z_]{0,31})\\b.*");
 
-		regEx.put(TokenType.Double, "\\b(double)\\b.*");
-		regEx.put(TokenType.String, "\\b(String)\\b.*");
-		regEx.put(TokenType.Float, "\\b(float)\\b.*");
-		regEx.put(TokenType.WhiteSpace, "( ).*");
-		regEx.put(TokenType.IntValue, "\\[0-9]{1}[0-9]*\\.*");
+		regEx.put(TokenType.Double, ".*\\b(double)\\b.*");
+		regEx.put(TokenType.String, ".*\\b(string)\\b.*");
 		
-		regEx.put(TokenType.While, "\\b(while)\\b.*");
+
+		regEx.put(TokenType.Float, ".*\\b(float)\\b.*");
 		
-		regEx.put(TokenType.For, "\\b(for)\\b.*");
-		regEx.put(TokenType.If, "\\b(if)\\b.*");
-		regEx.put(TokenType.Then, "\\b(then)\\b.*");
-		regEx.put(TokenType.ElseIF, "\\b(elsif)\\b.*");
-		regEx.put(TokenType.Else, "\\b(else)\\b.*");
-		regEx.put(TokenType.EqualEqual, "(==).*");
-		regEx.put(TokenType.NotEqual, "(\\!=).*");
-		regEx.put(TokenType.Assignment, "(=).*");
-		regEx.put(TokenType.Null, "\\b(null)\\b.*");
-		regEx.put(TokenType.Semicolon, "(;).*");
-		regEx.put(TokenType.Comma, "(,).*");
-		regEx.put(TokenType.PlusPlus, "(\\++).*");
-		regEx.put(TokenType.MinusMinus, "(\\--).*");
-		regEx.put(TokenType.Plus, "(\\+{1}).*");
-		regEx.put(TokenType.Minus, "(\\-{1}).*");
-		regEx.put(TokenType.Multiply, "(\\*).*");
-		regEx.put(TokenType.Divide, "(/).*");
-		regEx.put(TokenType.Mod, "(%).*");
+		regEx.put(TokenType.FloatDouble, ".*\\(\\d+)\\.(\\d+)?.*");
+		regEx.put(TokenType.WhiteSpace, ".*( ).*");
+		regEx.put(TokenType.IntValue, ".*\\^([+-])?([0-9]+)\\$.*");
+		
+		regEx.put(TokenType.While, ".*\\b(while)\\b.*");
+		
+		regEx.put(TokenType.For, ".*\\b(for)\\b.*");
+		regEx.put(TokenType.If, ".*\\b(if)\\b.*");
+		regEx.put(TokenType.Then, ".*\\b(then)\\b.*");
+		regEx.put(TokenType.ElseIF, ".*\\b(elsif)\\b.*");
+		regEx.put(TokenType.Else, ".*\\b(else)\\b.*");
+		regEx.put(TokenType.EqualEqual, ".*(==).*");
+		regEx.put(TokenType.NotEqual, ".*(\\!=).*");
+		regEx.put(TokenType.Assignment, ".*(=).*");
+		regEx.put(TokenType.Null, ".*\\b(null)\\b.*");
+		regEx.put(TokenType.Semicolon, ".*(;).*");
+		regEx.put(TokenType.Comma, ".*(,).*");
+		regEx.put(TokenType.PlusPlus, ".*(\\++).*");
+		regEx.put(TokenType.MinusMinus, ".*(\\--).*");
+		regEx.put(TokenType.Plus, ".*(\\+{1}).*");
+		regEx.put(TokenType.Minus, ".*(\\-{1}).*");
+		regEx.put(TokenType.Multiply, ".*(\\*).*");
+		regEx.put(TokenType.Divide, ".*(/).*");
+		regEx.put(TokenType.Mod, ".*(%).*");
 		regEx.put(TokenType.And, "(\\&&).*");
 		
 		
 		
-		regEx.put(TokenType.Or, "(\\||).*");
+		regEx.put(TokenType.Or, ".*(\\|{2}).*");
 		
 		
-		regEx.put(TokenType.GreaterOrEqual, "(>=).*");
-		regEx.put(TokenType.LessOrEqual, "(<=).*");
-		regEx.put(TokenType.Greater, "(>).*");
-		regEx.put(TokenType.Less, "(<).*");
-		regEx.put(TokenType.Break, "\\b(break)\\b.*");
+		regEx.put(TokenType.GreaterOrEqual, ".*(>=).*");
+		regEx.put(TokenType.LessOrEqual, ".*(<=).*");
+		regEx.put(TokenType.Greater, ".*(>).*");
+		regEx.put(TokenType.Less, ".*(<).*");
+		regEx.put(TokenType.Break, ".*\\b(break)\\b.*");
 		
 		
-		regEx.put(TokenType.Static, "\\b(static)\\b.*");
-		regEx.put(TokenType.Void, "\\b(void)\\b.*");
-		regEx.put(TokenType.Public, "\\b(public)\\b.*");
-		regEx.put(TokenType.Default, "\\b(default)\\b.*");
-		regEx.put(TokenType.Continue, "\\b(continue)\\b.*");
-		regEx.put(TokenType.Return, "\\b(return)\\b.*");
-		regEx.put(TokenType.Long, "\\b(long)\\b.*");
-		regEx.put(TokenType.Char, "\\b(char)\\b.*");
-		regEx.put(TokenType.Boolean, "\\b(boolean)\\b.*");
-		regEx.put(TokenType.Class, "\\b(class)\\b.*");
-		regEx.put(TokenType.Switch, "\\b(switch)\\b.*");
-		regEx.put(TokenType.Case, "\\b(case)\\b.*");
-		regEx.put(TokenType.BlockComment, "(/\\*.*?\\*/).*");
-		regEx.put(TokenType.LineComment, "(//(.*?)[\r$]?\n).*");
+		regEx.put(TokenType.Static, ".*\\b(static)\\b.*");
+		regEx.put(TokenType.Void, ".*\\b(void)\\b.*");
+		regEx.put(TokenType.Public, ".*\\b(public)\\b.*");
+		regEx.put(TokenType.Default, ".*\\b(default)\\b.*");
+		regEx.put(TokenType.Continue, ".*\\b(continue)\\b.*");
+		regEx.put(TokenType.Return, ".*\\b(return)\\b.*");
+		regEx.put(TokenType.Long, ".*\\b(long)\\b.*");
+		regEx.put(TokenType.Char, ".*\\b(char)\\b.*");
+		regEx.put(TokenType.Boolean, ".*\\b(boolean)\\b.*");
+		regEx.put(TokenType.Class, ".*\\b(class)\\b.*");
+		regEx.put(TokenType.Switch, ".*\\b(switch)\\b.*");
+		regEx.put(TokenType.Case, ".*\\b(case)\\b.*");
+		regEx.put(TokenType.BlockComment, ".*(/\\*.*?\\*/).*");
+		regEx.put(TokenType.LineComment, ".*(//(.*?)[\r$]?\n).*");
 		
-		regEx.put(TokenType.Tab, "(\\t).*");
-		regEx.put(TokenType.NewLine, "(\\n).*");
-		regEx.put(TokenType.CloseBrace, "(\\)).*");
-		regEx.put(TokenType.OpenBrace, "(\\().*");
-		regEx.put(TokenType.OpeningCurlyBrace, "(\\{).*");
-		regEx.put(TokenType.ClosingCurlyBrace, "(\\}).*");
-		regEx.put(TokenType.True, "\\b(true)\\b.*");
-		regEx.put(TokenType.False, "\\b(false)\\b.*");
+		regEx.put(TokenType.Tab, ".*(\\t).*");
+		regEx.put(TokenType.NewLine, ".*(\\n).*");
+		regEx.put(TokenType.CloseBrace, ".*(\\)).*");
+		regEx.put(TokenType.OpenBrace, ".*(\\().*");
+		regEx.put(TokenType.OpeningCurlyBrace, ".*(\\{).*");
+		regEx.put(TokenType.ClosingCurlyBrace, ".*(\\}).*");
+		regEx.put(TokenType.True, ".*\\b(true)\\b.*");
+		regEx.put(TokenType.False, ".*\\b(false)\\b.*");
+		regEx.put(TokenType.StringValue, ".*\\\"([^\"]*)\"\\.*");
+		regEx.put(TokenType.Identifier, ".*\\b([a-zA-Z]{1}[0-9a-zA-Z_]{0,31})\\b.*");
+
 //		regEx.put(TokenType.Identifier, "\\b([a-zA-Z]{1}[0-9a-zA-Z_]{0,31})\\b.*");
 
 
-
+		
 		
 		
 		
@@ -216,13 +410,6 @@ public class Lexer {
 		
 		
 		//regEx.put(TokenType.Point, "(\\.).*");
-		
-		
-		
-		
-		
-		
-		
-		
+			
 	}
 }
